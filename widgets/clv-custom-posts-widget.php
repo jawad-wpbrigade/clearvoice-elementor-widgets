7c0f4ce0fc671b714b55a2439c9c8b9fd846b747
jawad-wpbrigade <jawad.wpbrigade@gmail.com>
2022-12-29 15:56:40 +0500

    chore: Select Categories Title Added

--- widgets/clv-custom-posts-widget.php
+++ widgets/clv-custom-posts-widget.php
@@ -1,5 +1,4 @@
 <?php
-namespace ClearVoice\ElementorWidgets\Widgets;
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
@@ -79,7 +78,7 @@ class  Clearvoice_Custom_Posts extends \Elementor\Widget_Base {
 			'content_section',
 			array(
 				'label' => esc_html__( 'Content', 'ClearVoice-elementor-widgets' ),
-				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+				'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
 			)
 		);
 
@@ -90,8 +89,8 @@ class  Clearvoice_Custom_Posts extends \Elementor\Widget_Base {
 		$args = array(
     		'hide_empty' => false,
 		);
-		$categories = get_categories($args);
-		if( isset( $categories ) && is_array( $categories ) ) {
+		$categories = get_categories( $args );
+		if ( isset( $categories ) && is_array( $categories ) ) {
 			foreach ( $categories as $key => $category ) {
 				$options[ $category->name ] = $category->name;
 			}
@@ -99,35 +98,36 @@ class  Clearvoice_Custom_Posts extends \Elementor\Widget_Base {
         $this->add_control(
 			'clv_categories',
 			array(
-				'label' => esc_html__( 'Select Categories', 'ClearVoice-elementor-widgets' ),
-				'type' => \Elementor\Controls_Manager::SELECT2,
+				'label'       => esc_html__( 'Select Categories', 'ClearVoice-elementor-widgets' ),
+				'type'        => \Elementor\Controls_Manager::SELECT2,
 				'label_block' => true,
-				'multiple' => true,
-				'options' =>  $options,
+				'multiple'    => true,
+				'options'     =>  $options,
 			)
 		);
 		$this->add_control(
 			'clv_posts_per_page',
-			[
-				'label' => esc_html__( 'Posts Per Page', 'ClearVoice-elementor-widgets' ),
-				'type' => \Elementor\Controls_Manager::NUMBER,
-				'min' => -1,
-			]
+			array(
+				'label'       => esc_html__( 'Posts Per Page', 'ClearVoice-elementor-widgets' ),
+				'type'        => \Elementor\Controls_Manager::NUMBER,
+				'min'         => -1,
+				'separator'   => 'before',
+			)
 		);
 		$this->add_control(
 			'clv_hide_posts',
-			[
+			array(
 				'label' => esc_html__( 'Hide Posts', 'ClearVoice-elementor-widgets' ),
-				'type' => \Elementor\Controls_Manager::TEXT,
-			]
+				'type'  => \Elementor\Controls_Manager::TEXT,
+			)
 		);
 		$this->add_control(
 			'clv_hide_posts_description',
-			[
+			array(
 				'raw' => esc_html__( " Enter the comma separated post id's that you want to hide like : 23,43 ", 'elementor' ),
 				'type' => \Elementor\Controls_Manager::RAW_HTML,
 				'content_classes' => 'elementor-descriptor',
-			]
+			)
 		);
 
 		$this->end_controls_section();
@@ -144,8 +144,8 @@ class  Clearvoice_Custom_Posts extends \Elementor\Widget_Base {
 	 */
 	protected function render() {
 		$settings          = $this->get_settings_for_display();
-		$clv_post_per_page = $settings['clv_posts_per_page'];
-		$clv_categories    = $settings['clv_categories'];
+		$clv_post_per_page = isset( $settings['clv_posts_per_page'] ) ? $settings['clv_posts_per_page'] : -1;
+		$clv_categories    = isset( $settings['clv_categories'] ) ? $settings['clv_categories'] : array();
 		$clv_hide_posts    = explode( ',' , $settings['clv_hide_posts'] );
 
 		// Let's do the query magic now.
@@ -163,19 +163,19 @@ class  Clearvoice_Custom_Posts extends \Elementor\Widget_Base {
 			'order'          => 'ASC',
 		);
 
-		$query = new \WP_Query($args);
+		$query = new WP_Query($args);
 		if ( $query->have_posts() ) {
 			while( $query->have_posts() ) {
 				$query->the_post(); 
 				?>
 				<div style = "max-wdith: 50px">
-				<div><?php echo get_the_post_thumbnail( get_the_ID(), array( 200, 200) ) ?></div>
+				<div><?php echo get_the_post_thumbnail( get_the_ID(), array( 200, 200 ) ); ?></div>
 				<p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
 				</div>
 				<?php
 			}
 		}
 		// Reset Query.
-        wp_reset_query() ;
+		wp_reset_query() ;
 	}
 }
